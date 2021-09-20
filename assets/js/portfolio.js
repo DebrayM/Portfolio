df2cf7ce19d58c36c70f5c885871066e6af1a653
Martine Debray <m.debray@codeur.online>
2021-09-20 16:57:43 +0200

    nouvelle version Design responsive et NAV

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -30,10 +30,10 @@ function verifEmail() {
 }
 
 // Expression régulière permettant la vérification syntaxique du nom
-function checkNom(email)
+function checkNom(nom)
 {
     const re = /^([a-zA-Z ]+)$/;
-    return re.test(email);
+    return re.test(nom);
 }
 
 function verifNom() {
@@ -52,63 +52,11 @@ function verifNom() {
         this.style.borderColor = 'red';
         if (this.nextSibling.length) {
             this.after(error);
-        }
-             
+        }       
     }
-  
 }
 
-function isMobileDevice() { 
-    if( navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-    ){
-       return true;
-     }
-    else {
-       return false;
-     }
-}
-function Gdporiginal(){
-    let txt = document.getElementById("demo1");
-    txt.remove();
-    document.getElementById("purpleBubble1").style.transform = "";
-    let leTexte = '<p class="monTexte">Gestion de projet</p>';
-    document.getElementById("purpleBubble1").innerHTML = leTexte;
-}
-function Gdp(){
-    this.style.transform = "perspective(500px) translate3d(10px,0px,100px)";
-    let leTexte = "<p id='demo1'>Plus de 20 années d'expérience en gestion de projet</p>";
-    this.innerHTML = leTexte;
-    let txt = document.getElementById("demo1"); 
-    txt.style.textAlign = "center";
-    txt.style.fontSize = "1.8em";
-    txt.style.paddingTop = "5vh";
-}
-function Mtdoriginal(){
-    let txt = document.getElementById("demo2");
-    txt.remove();
-    document.getElementById("purpleBubble2").style.transform = "";
-    let leTexte = '<p class="monTexte">Méthodes</p>';
-    document.getElementById("purpleBubble2").innerHTML = leTexte;
-}
-function Mtd(){
-    this.style.transform = "perspective(500px) translate3d(10px,0px,100px)";
-    let leTexte = "<p id='demo2'>Coach Agile et Scrum Master, PMO, Formatrice en Méthodologie (V, W)</p>";
-    this.innerHTML = leTexte;
-    let txt = document.getElementById("demo2"); 
-    txt.style.textAlign = "center";
-    txt.style.fontSize = "1.8em";
-    txt.style.paddingTop = "5vh";
-}
+
 window.onload = function() {
-    document.getElementById("purpleBubble1").addEventListener("mouseenter", Gdp);
-    document.getElementById("purpleBubble2").addEventListener("mouseenter", Mtd);
 
-    document.getElementById("purpleBubble1").addEventListener("mouseout", Gdporiginal);
-    document.getElementById("purpleBubble2").addEventListener("mouseout", Mtdoriginal); 
 }
